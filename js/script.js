1f93040beaa80e04993ca37dd7e4101f0090994c
nikotlichnik <shtreisnikita@gmail.com>
2018-02-14 18:26:24 +0300

    Добавление файлов для слайдера

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel(
-        {
-            loop:true,
-            margin:10,
-            nav:true,
-            items:5
-        }
-    );
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+      0:{
+        items:1
+      },
+      600:{
+        items:3
+      },
+      1000:{
+        items:5
+      }
+    }
+  })
 });
