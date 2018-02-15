2eaf02dd339fdf1a49de99c41bfe879266d16561
nikotlichnik <shtreisnikita@gmail.com>
2018-02-15 13:04:47 +0300

    Адаптация всей страницы под ширину меньше 768px

--- build/js/script.js
+++ build/js/script.js
@@ -1,10 +1,25 @@
-$(document).ready(function(){
-  $('.news__items').owlCarousel({
-    loop:true,
-    nav: true,
-    navText: [$('.news__toggle--next'), $('.news__toggle--previous')],
-    controlsClass: 'news__toggles',
-    navClass: ['news__toggle--next','news__toggle--previous']
-  })
+// $(document).ready(function(){
+//   $('.news__items').owlCarousel({
+//     loop:true,
+//     nav: true,
+//     navText: [$('.news__toggle--next'), $('.news__toggle--previous')],
+//     controlsClass: 'news__toggles',
+//     navClass: ['news__toggle--next','news__toggle--previous']
+//   })
+//
+// });
 
+var toggle = document.querySelector(".main-nav__toggle");
+var nav = document.querySelector(".main-nav");
+
+toggle.addEventListener("click", function () {
+  if (nav.classList.contains("main-nav--closed")) {
+    nav.classList.remove("main-nav--closed");
+    nav.classList.add("main-nav--opened");
+  } else {
+    if (nav.classList.contains("main-nav--opened")) {
+      nav.classList.remove("main-nav--opened");
+      nav.classList.add("main-nav--closed");
+    }
+  }
 });
